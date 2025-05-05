fe32321c691a9afbeb90d47a93ea96b6ee0e1edf
samuelkaranja <samuelkaranja007@gmail.com>
2025-05-05 23:56:26 +0300

    Added box shadow styling in the post card

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,19 +1,17 @@
-import { useContext } from "react";
 import "./post.css";
 import { CiHeart } from "react-icons/ci";
 import { Link } from "react-router-dom";
-import { GlobalContext } from "../../context/context";
 
 const Post = ({ blog }) => {
   const slug = blog?.title.replace(/\s+/g, "-"); // Replace spaces with dashes
-  const { handleDelete } = useContext(GlobalContext);
 
   return (
     <div className="post">
       <img src={blog?.image} alt="" />
       <div className="body">
         <div className="dl">
-          <small>
+          <small style={{ fontSize: "11px" }}>
+            Created On:{" "}
             {new Date(blog?.created_at).toLocaleDateString("en-US", {
               year: "numeric",
               month: "long",
@@ -34,19 +32,6 @@ const Post = ({ blog }) => {
         </p>
         <small className="author">author: {blog?.author}</small>
       </div>
-      {/* <button
-        onClick={() => handleDelete(blog.id)}
-        style={{
-          background: "red",
-          color: "white",
-          border: "none",
-          padding: "0.5rem 1rem",
-          borderRadius: "5px",
-          cursor: "pointer",
-        }}
-      >
-        Delete
-      </button> */}
     </div>
   );
 };
